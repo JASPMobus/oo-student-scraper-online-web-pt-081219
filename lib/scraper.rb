15350849f89cef9227847f66d417b629112b3791
James Mobus <mobusj@live.com>
2019-10-16 20:25:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
   def self.scrape_profile_page(profile_url) 
     socials = Nokogiri::Slop(open(profile_url)).css("div.social-icon-container a")
     quote = Nokogiri::HTML(open(profile_url)).css("div.profile-quote")
-    bio = Nokogiri::HTML(open(profile_url)).css("div.description-holder")
+    bio = Nokogiri::HTML(open(profile_url)).css("div.description-holder p")
     
     {
       :twitter => socials[0]["href"], 
