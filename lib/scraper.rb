907439016e839ce2891c944436da55d8b84745c0
James Mobus <mobusj@live.com>
2019-10-16 20:01:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    students = Nokogiri::HTML(open())
+    students = Nokogiri::HTML(open(index_url)).css("div.")
   end
 
   def self.scrape_profile_page(profile_url)
