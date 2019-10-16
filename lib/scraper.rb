aae6699718718edea033ef8aa934956755abd57f
James Mobus <mobusj@live.com>
2019-10-16 20:03:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    ret_array = []
     students = Nokogiri::HTML(open(index_url)).css("div.student-card a")
-    
-    []
   end
 
   def self.scrape_profile_page(profile_url)
