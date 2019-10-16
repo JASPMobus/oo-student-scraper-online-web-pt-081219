06fe60ef2b2a54bda2773de074f8eb7b4856b847
James Mobus <mobusj@live.com>
2019-10-16 20:02:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    students = Nokogiri::HTML(open(index_url)).css("div.student-card")
+    students = Nokogiri::HTML(open(index_url)).css("div.student-card a")
   end
 
   def self.scrape_profile_page(profile_url)
