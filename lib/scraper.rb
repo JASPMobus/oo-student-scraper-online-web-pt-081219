5288b733ba9d64be9924fc2ce12d408fe74a5c1c
James Mobus <mobusj@live.com>
2019-10-16 20:12:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
     students.each do |student|
       student_hash = {
         :name => student["div.card-text-container h4"].text,
-        :location => student["div.card-text-container p"].text
+        :location => student["div.card-text-container"].text
         :profile_url => student["href"]
       }
       
